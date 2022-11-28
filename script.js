0784976ed57c9a232dd9ebb83a66c26d3163aa64
Sophie Vercoutter <webdesigner@sitify.fr>
2022-11-28 13:48:33 +0100

    bootstrap new site

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("scroll", (e) => {
+  let btnScrollTop = document.getElementById("scrollTop");
+  if (window.scrollY > 100) {
+    btnScrollTop.style.display = "initial";
+  } else {
+    btnScrollTop.style.display = "none";
+  }
+});

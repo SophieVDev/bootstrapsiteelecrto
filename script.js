31dc8d67630f8d93879b98bc8529da7480baf7d7
Sophie Vercoutter <webdesigner@sitify.fr>
2022-11-28 14:59:09 +0100

    new header

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll", (e) => {
   let btnScrollTop = document.getElementById("scrollTop");
-  if (window.scrollY > 100) {
+  if (window.scrollY >= 100) {
     btnScrollTop.style.display = "initial";
   } else {
     btnScrollTop.style.display = "none";
